503759c9ee287e1487e622938f424c5ae2a6d252
Hawkynt <github@synthelicz.de>
2025-06-05 13:18:32 +0200

    Remove unused PlaylistMock

---
+++ Tests/EditorTests/MinimalExtensions.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BeatSaber_Playlist_Editor.ViewModel {
+  internal static class MinimalExtensions {
+    public static bool SetProperty<T>(this object owner, System.Action<string?>? changed, ref T field, T value, [CallerMemberName] string? propertyName = null) {
+      if (EqualityComparer<T>.Default.Equals(field, value))
+        return false;
+      field = value;
+      changed?.Invoke(propertyName);
+      return true;
+    }
+
+    public static void AddRange<T>(this IList<T> list, IEnumerable<T> items) {
+      foreach (var item in items)
+        list.Add(item);
+    }
+
+    public static bool ContainsNot<T>(this ICollection<T> collection, T item) => !collection.Contains(item);
+
+    public static bool IsNotNullOrWhiteSpace(this string? text) => !string.IsNullOrWhiteSpace(text);
+  }
+
+  public class SortableBindingList<T> : BindingList<T> {
+    public SortableBindingList() { }
+    public SortableBindingList(IList<T> list) : base(list) { }
+  }
+}
